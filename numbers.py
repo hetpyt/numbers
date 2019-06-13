ddf6f06ac5d59e1edb1294e874584014fece9d34
hetpyt <43849401+hetpyt@users.noreply.github.com>
2019-06-13 15:53:54 +0300

    add units support

--- numbers.py
+++ numbers.py
@@ -62,12 +62,15 @@ class Numbers:
             9 : "девятьсот"
         }
     }
+    # суффиксы частей кратных 1000
     _10multiple_suffix = {
         4 : ("тысяча", "тысячи","тысяч"),
         7 : ("миллион", "миллиона", "миллионов"),
         10 : ("миллиард", "миллиарда", "миллиардов")
     }
-    _beforedot_suffix = ("целая", "целые", "целых")
+    # суффикс целой части
+    _integer_suffix = ("целая", "целые", "целых")
+    # суффиксы дробной части (до 5 цифр после запятой)
     _fractional_suffix = {
         1 : ("десятая", "десятые", "десятых"),
         2 : ("сотая", "сотые", "сотых"),
@@ -75,7 +78,6 @@ class Numbers:
         4 : ("десятитысячная", "десятитысячные", "десятитысячных"),
         5 : ("стотысячная", "стотысячные", "стотысячных")
     }
-    
     
     def __init__(self):
         _PyMajorVersion = sys.version_info[0]
@@ -85,14 +87,22 @@ class Numbers:
             return isinstance(value, str)
         else:
             return isinstance(value, basestring)
-     
+
+    def _choose_suffix(self, suffixes, digit):
+        if digit == 1:
+            return suffixes[0]
+        elif digit > 1 and digit < 5:
+            return suffixes[1]
+        else:
+            return suffixes[2]
+    
     def _convert(self, result, num_part, gender = 0):
         # счетчик позиции (разряда) цифры в числе начиная с 1 от десятичной точки справа налево
         pos = len(num_part)
         if pos == 1 and int(num_part) == 0:
             # частный случай нуля
             result.append(self._zero)
-            return result
+            return 0
         # признак начала последовательности из 1 и [0,9] начиная в позиции десяток
         # те для обработки чисел 11, 12 и тд до 19, тк тут два цифры идут одним словом
         is_11 = False
@@ -103,22 +113,20 @@ class Numbers:
             num_class = pos % 3
             # текстовое представление текущего разряда
             str_digit = ""
-            int_suffix = ""
+            suffix = ""
             # определим суффикс
             if pos in self._10multiple_suffix:
-                if digit == 1 and not is_11:
-                    int_suffix = self._10multiple_suffix[pos][0]
-                elif digit > 1 and digit < 5 and not is_11:
-                    int_suffix = self._10multiple_suffix[pos][1]
+                if is_11:
+                    suffix = self._10multiple_suffix[pos][2]
                 else:
-                    int_suffix = self._10multiple_suffix[pos][2]
+                    suffix = self._choose_suffix(self._10multiple_suffix[pos], digit)
             # определим представление        
             if is_11:
                 # была единица на классе 2 в предыдущей итерации
                 str_digit = self._number_class[2][1][digit]
-                if pos in self._10multiple_suffix:
-                    int_suffix = self._10multiple_suffix[pos][2]
                 is_11 = False
+                # установим digit в двухциферное число чтобы вернуть его из функции если оно последнее
+                digit += 10
             else:
                 if digit == 1 and num_class == 2:
                     # частный случай для десяток (10, 11, 12 и тд)
@@ -141,29 +149,54 @@ class Numbers:
             if str_digit:
                 result.append(str_digit) 
             # добавим суффикс для 10, 7, 4 разрядов
-            if int_suffix:
-                result.append(int_suffix) 
+            if suffix:
+                result.append(suffix) 
             # декремент счетчика разрядов
             pos -= 1
         # возврат результата
-        return result 
+        return digit 
         
-    def convert(self, number, gender = "masculine"):
+    def convert(self, number, int_gender = 0, fract_gender = 0, unit = None, int_unit = None, fract_unit = None):
         # преобразуем к числу с плавающей точкой и округляем до 5 знаков после запятой
         number = round(float(number), 5)
         # проверка на размерность числа
         if number > self._max_number:
             return None
-        # проверим вдруг в качестве рода передали строковое представление рода
-        if self._is_string(gender):
-            gender = self._genders.index(gender)
         # разобьем число на целую и дробную части
         int_part, fract_part = tuple(format(number).split("."))
+        non_zero_fract_part = 0 != int(fract_part)
         # список результата
         result = []
-        self._convert(result, int_part, gender)
-        #TODO пока что
-        result.append("точка")
-        self._convert(result, fract_part, gender)
+        if None == int_unit:
+            # не задана единица для целой части
+            if non_zero_fract_part:
+                # и есть ненулевая дробная часть
+                # подставляем стандартный суффикс "целая" и меняем род на женский
+                int_unit = self._integer_suffix
+                int_gender = 1
+            else:
+                # нет дробной части - нет единицы
+                int_unit = ("", "", "")
+        # последняя цифра целой части
+        last_int_digit = self._convert(result, int_part, int_gender)
+        result.append(self._choose_suffix(int_unit, last_int_digit))
+        if 0 != int(fract_part):
+            # конвертируем только ненулевые дробные части
+            if None == fract_unit:
+                # не задана единица для дробной части
+                # подставляем стандартные
+                fract_unit = self._fractional_suffix[len(fract_part)]
+                fract_gender = 1
+            result.append(self._choose_suffix(fract_unit, self._convert(result, fract_part, fract_gender)))
+        # общая единица измерения
+        if None != unit:
+            # передана общая единица измерения - она ставится в конец
+            if non_zero_fract_part:
+                # для ненулевой дробной части берется вторая форма, пр "одна целая одна сотая литра"
+                result.append(unit[1])
+            else:
+                # для нулевой дробной части форма определяется по последней цифре целой
+                result.append(self._choose_suffix(unit, last_int_digit))
         # возврат результата
         return result
+        
